c6c22147b582f567baac67059b8517be2b53c1ff
bkandh30 <kandhari.bhavya20@gmail.com>
2026-03-19 18:02:53 -0700

    Added middleware for session check

---
+++ src/middleware/auth.ts
@@ -0,0 +1,17 @@
+import { createMiddleware } from '@tanstack/react-start'
+import { auth } from '@/lib/auth'
+import { getRequestHeaders } from '@tanstack/react-start/server'
+import { redirect } from '@tanstack/react-router'
+
+export const authFnMiddleware = createMiddleware({ type: 'function' }).server(
+    async ({ next }) => {
+        const headers = getRequestHeaders()
+        const session = await auth.api.getSession({ headers })
+
+        if (!session) {
+            throw redirect({ to: '/login' })
+        }
+
+        return next({ context: { session }})
+    }
+)
