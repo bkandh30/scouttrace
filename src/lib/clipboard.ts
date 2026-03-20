cd8031167c2d322c425af0ec30237a8fb38c52c6
bkandh30 <kandhari.bhavya20@gmail.com>
2026-03-20 15:43:37 -0700

    Added clipboard helper function for copying URLs

---
+++ src/lib/clipboard.ts
@@ -0,0 +1,8 @@
+import { createClientOnlyFn } from '@tanstack/react-start'
+import { toast } from 'sonner'
+
+export const copyToClipboardFn = createClientOnlyFn(async (url: string) => {
+    await navigator.clipboard.writeText(url)
+    toast.success('URL copied to clipboard')
+    return
+})
